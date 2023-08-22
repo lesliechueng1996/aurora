87afc94dce9bdabf41cacce60af68637966e07b8
LeslieCheung <lesliechueng1996@icloud.com>
2023-08-22 10:55:56 +0800

    update log format

--- aurora-server/src/main.ts
+++ aurora-server/src/main.ts
@@ -3,44 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionFilter } from './filter/any-exception.filter';
-import { WinstonModule } from 'nest-winston';
-import { format, transports } from 'winston';
-import 'winston-daily-rotate-file';
+import loggerConfig from './config/logger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        // error log
-        new transports.DailyRotateFile({
-          filename: 'logs/error/%DATE%-error.log',
-          level: 'error',
-          format: format.combine(format.timestamp(), format.json()),
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: false,
-          maxFiles: '30d',
-        }),
-        // all level log
-        new transports.DailyRotateFile({
-          filename: 'logs/all/%DATE%-all.log',
-          format: format.combine(format.timestamp(), format.json()),
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: false,
-          maxFiles: '30d',
-        }),
-        // console log
-        new transports.Console({
-          format: format.combine(
-            format.cli(),
-            format.splat(),
-            format.timestamp(),
-            format.printf((info) => {
-              return `${info.timestamp} [${info.level}] ${info.message}`;
-            }),
-          ),
-        }),
-      ],
-    }),
+    logger: loggerConfig,
   });
 
   app.useGlobalPipes(new ValidationPipe());
